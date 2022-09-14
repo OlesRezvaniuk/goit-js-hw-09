64bf77bb24f60e01024c130d300d8b49510bce68
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-09-14 13:34:30 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,9 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const delayForm = document.querySelector('.form');
-delayForm.addEventListener('submit', getPromises);
+const delayForm = document.querySelector('.form'); // Отримуємо доступ до елемента форми
+delayForm.addEventListener('submit', getPromises); // Створюємо слухача на формі
 
+//Створюємо функцію яка викликає проміси
 function getPromises(event) {
   let step = Number(this.step.value);
   let amount = Number(this.amount.value);
@@ -12,18 +13,18 @@ function getPromises(event) {
 
   event.preventDefault();
 
-  const generatorCounter = setInterval(() => {
+  const promiseGen = setInterval(() => {
     count += 1;
     delayForEach += step;
 
     createPromise(count, delayForEach).then(showSucces).catch(showError);
 
     if (count === amount) {
-      clearInterval(generatorCounter);
+      clearInterval(promiseGen);
     }
   }, step);
 }
-
+// Створюємо функцію яка повертає новий проміс з затримкою
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
