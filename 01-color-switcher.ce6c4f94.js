9e53defc594a638ea5eb9a46311e7bb91449bd4b
OlesRezvaniuk <105782635+OlesRezvaniuk@users.noreply.github.com>
2022-09-14 08:01:45 +0000

    Deploying to gh-pages from @ OlesRezvaniuk/goit-js-hw-09@fa5f3461451b6a1ea2ed351e6e99b7215544b4d9 🚀

--- 01-color-switcher.ce6c4f94.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},e=document.body;t.startBtn.addEventListener("click",(()=>{t.startBtn.disabled="disabled",t.stopBtn.removeAttribute("disabled"),intervalId=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(()=>{!function(e){t.startBtn.removeAttribute("disabled"),t.stopBtn.disabled="disabled",clearInterval(e)}(intervalId)}));
-//# sourceMappingURL=01-color-switcher.ce6c4f94.js.map
